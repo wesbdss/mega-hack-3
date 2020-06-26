40480601443abdeef714100b26e702ac990dd01a
wesbdss <wesleybenicio4@gmail.com>
2020-06-25 23:11:31 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';;
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <img src="./loliWisky.jpg" alt="" srcset=""/>
         <a
           className="App-link"
           href="https://reactjs.org"
