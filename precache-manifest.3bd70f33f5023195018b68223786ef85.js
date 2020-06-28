c653fdf994cfc2060619897529360447f1174c1f
wesbdss <wesleybenicio4@gmail.com>
2020-06-28 09:43:49 -0300

    Updates

---
+++ precache-manifest.3bd70f33f5023195018b68223786ef85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f822e1b274a5fc892b7d06dc89a11e",
+    "url": "/mega-hack-3/index.html"
+  },
+  {
+    "revision": "e17ad9e117e144c1bd59",
+    "url": "/mega-hack-3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c776d534b1ada3795d60",
+    "url": "/mega-hack-3/static/js/2.64481f3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mega-hack-3/static/js/2.64481f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17ad9e117e144c1bd59",
+    "url": "/mega-hack-3/static/js/main.47f16eef.chunk.js"
+  },
+  {
+    "revision": "c17380bcb49eb1d8979d",
+    "url": "/mega-hack-3/static/js/runtime-main.3b672b39.js"
+  }
+]);
