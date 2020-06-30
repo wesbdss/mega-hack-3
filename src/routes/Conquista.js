e4aef658d9005391e719c1bbe9f9340c9b606d91
wesbdss <wesleybenicio4@gmail.com>
2020-06-30 13:08:46 -0300

    add image

--- src/routes/Conquista.js
+++ src/routes/Conquista.js
@@ -5,6 +5,7 @@ export default class Conquista extends Component {
         return (
             <div style={{color:"Black"}}>
                 Card PAGE
+                <img src="https://media1.giphy.com/media/xUNd9Ljg37yeVcCHyE/giphy.gif" alt=""/>
             </div>
         )
     }
