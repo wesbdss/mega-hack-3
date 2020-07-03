97b497e306fee302ae2928bdb3044327cd75b945
wesbdss <wesleybenicio4@gmail.com>
2020-07-03 11:45:48 -0300

    Updates

---
+++ precache-manifest.7971a4567226c3f204609e9c068a0353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065a66716eafeefa16f8068c92efdfff",
+    "url": "/mega-hack-3/index.html"
+  },
+  {
+    "revision": "7074360f31d0b04b1ef5",
+    "url": "/mega-hack-3/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1cabdf52e4944c22668",
+    "url": "/mega-hack-3/static/css/main.8e1d4599.chunk.css"
+  },
+  {
+    "revision": "7074360f31d0b04b1ef5",
+    "url": "/mega-hack-3/static/js/2.341744f2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/mega-hack-3/static/js/2.341744f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cabdf52e4944c22668",
+    "url": "/mega-hack-3/static/js/main.7f566ae3.chunk.js"
+  },
+  {
+    "revision": "c17380bcb49eb1d8979d",
+    "url": "/mega-hack-3/static/js/runtime-main.3b672b39.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mega-hack-3/precache-manifest.f8e01cbe7a6b8e1cd376b8426336c9b5.js"
+  "/mega-hack-3/precache-manifest.7971a4567226c3f204609e9c068a0353.js"
 );
 
 self.addEventListener('message', (event) => {
